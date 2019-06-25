df15c51a9c5dfc7e4c164c416210b9683983bc35
Natalia Tigh <natih6218@gmail.com>
2019-06-25 13:12:18 -0700

    add new list , and change text() to  show() to

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  $("form").submit(function(event) {
+    event.preventDefault();
+
+    var number = parseInt($("input#number").val());
+
+    for (var i = 1; i <= number; i+=1){
+      if (i % 3 === 0){
+        $("ul").append("<li> ping </li>");
+      } else if (i % 5 === 0){
+        $("ul").append("<li> pong</li>")
+      } else if (i % 15 === 0){
+        $("ul").append("<li> pingpong</li>")
+      } else {
+        $("ul").append("<li>"+ i  + "</li>")
+      }
+    }
+
+  });
+  $("#output").show(number);
+});
