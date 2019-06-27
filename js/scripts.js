486237e1ec2e0d84aa45e2f03756f193018d0685
Student <example@example.com>
2019-06-26 18:27:56 -0700

    add empty method

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,22 @@
 $(document).ready(function(){
   $("form").submit(function(event) {
+    $("#list").empty();
     event.preventDefault();
 
     var number = parseInt($("input#number").val());
 
     for (var i = 1; i <= number; i+=1){
       if (i % 3 === 0){
-        $("ul").append("<li> ping </li>");
+        $("ul#list").append("<li> ping </li>");
       } else if (i % 5 === 0){
-        $("ul").append("<li> pong</li>")
+        $("ul#list").append("<li> pong</li>")
       } else if (i % 15 === 0){
-        $("ul").append("<li> pingpong</li>")
+        $("ul#list").append("<li> pingpong</li>")
       } else {
-        $("ul").append("<li>"+ i  + "</li>")
+        $("ul#list").append("<li>"+ i  + "</li>")
       }
     }
   $("#output").show(number);
   });
 
-
-  this.remove();
 });
